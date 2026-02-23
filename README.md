23f0385baf0c5fe188e35ac04398ce4c22eca4ef
awakum <awakum192@gmail.com>
2026-02-23 21:35:40 +0300

    feat: hyprland only no dependencies requiered

--- README.md
+++ README.md
@@ -11,19 +11,10 @@ Plugin saves your actual layout before switching to English. The next time you e
 **It also works with focus.** When Neovim loses focus plugin switches your layout to the last saved one. When Neovim gets focus plugin saves your layout, which you could've changed in another window and switches to English **only if** you need it. ([Logic](#about))\
 Now you need to switch your layout only when you need to type something in a different language! That's the way it always should have been.
 
-## macOS
-1. Install [input-source-switcher](https://github.com/vovkasm/input-source-switcher)
-```bash
-git clone https://github.com/vovkasm/input-source-switcher.git
-cd input-source-switcher
-mkdir build && cd build
-brew install cmake
-cmake ..
-make
-sudo make install
-```
+## Hyprland Linux
+Unlike the master repository plugin, this one is intended for use with Hyprland only and therefore does not require any additional dependencies.
 
-2. Install this plugin
+1. Install this plugin
 <table>
 <tr>
   <th> Packer </th>
@@ -57,111 +48,7 @@ call dein#add('ivanesmantovich/xkbswitch.nvim')
 </tr>
 </table>
 
-3. Add the setup line to your config
-```lua
-require('xkbswitch').setup()
-```
-
-## Linux / Unix (X.org / Wayland)
-1. Install package `libxkbfile-dev` (or `libxkbfile-devel` if you use Fedora)
-2. Install [xkb-switch](https://github.com/grwlf/xkb-switch)
-```bash
-git clone https://github.com/grwlf/xkb-switch.git
-cd xkb-switch
-mkdir build && cd build
-cmake ..
-make
-sudo make install
-sudo ldconfig
-```
-3. Install this plugin
-<table>
-<tr>
-  <th> Packer </th>
-  <th> Lazy (~/.config/nvim/lua/plugins/xkbswitch.lua) </th>
-  <th> Dein </th>
-</tr>
-<tr>
-<td>
-
-```lua
-use 'ivanesmantovich/xkbswitch.nvim'
-```
-
-</td>
-<td>
-
-```lua
-return { 
-    {'ivanesmantovich/xkbswitch.nvim'} 
-}
-```
-
-</td>
-<td>
-
-```lua
-call dein#add('ivanesmantovich/xkbswitch.nvim')
-```
-
-</td>
-</tr>
-</table>
-
-4. Add the setup line to your config
-```lua
-require('xkbswitch').setup()
-```
-
-## GNOME 3 or 4 (g3kb-switch)
-1. Install `libglib2.0-dev` package (or `glib2-devel` if you use Fedora)
-2. Install [g3kb-switch](https://github.com/lyokha/g3kb-switch) and the [extension](https://github.com/lyokha/g3kb-switch#gnome-41-and-newer).
-```bash
-git clone https://github.com/lyokha/g3kb-switch.git
-cd g3kb-switch
-mkdir build && cd build
-cmake ..
-make
-sudo make install
-
-cd ../extension
-make install  # no sudo required!
-```
-3. Install this plugin
-<table>
-<tr>
-  <th> Packer </th>
-  <th> Lazy (~/.config/nvim/lua/plugins/xkbswitch.lua) </th>
-  <th> Dein </th>
-</tr>
-<tr>
-<td>
-
-```lua
-use 'ivanesmantovich/xkbswitch.nvim'
-```
-
-</td>
-<td>
-
-```lua
-return { 
-    {'ivanesmantovich/xkbswitch.nvim'} 
-}
-```
-
-</td>
-<td>
-
-```lua
-call dein#add('ivanesmantovich/xkbswitch.nvim')
-```
-
-</td>
-</tr>
-</table>
-
-4. Add the setup line to your config
+2. Add the setup line to your config
 ```lua
 require('xkbswitch').setup()
 ```
--- lua/xkbswitch.lua
+++ lua/xkbswitch.lua
@@ -6,73 +6,48 @@ M.events_get_focus = {'FocusGained', 'CmdlineLeave'}
 -- nvim_create_autocmd shortcut
 local autocmd = vim.api.nvim_create_autocmd
 
-local xkb_switch_lib = nil
-local user_os_name = vim.loop.os_uname().sysname
+local hyprland_main_keyboard = nil
+local user_us_layout_variation = nil
 
--- Find the path to the xkbswitch shared object (macOS)
-if user_os_name == 'Darwin' then
-    if vim.fn.filereadable('/usr/local/lib/libInputSourceSwitcher.dylib') == 1 then
-        xkb_switch_lib = '/usr/local/lib/libInputSourceSwitcher.dylib'
-    elseif vim.fn.filereadable('/usr/lib/libInputSourceSwitcher.dylib') == 1 then
-        xkb_switch_lib = '/usr/lib/libInputSourceSwitcher.dylib'
-    end
-    -- Find the path to the xkbswitch shared object (Linux)
-else
-    -- g3kb-switch
-    if vim.fn.filereadable('/usr/lib/libg3kbswitch.so') == 1 then
-        xkb_switch_lib = '/usr/lib/libg3kbswitch.so'
-    elseif vim.fn.filereadable('/usr/local/lib64/libg3kbswitch.so') == 1 then
-        xkb_switch_lib = '/usr/local/lib64/libg3kbswitch.so'
-    elseif vim.fn.filereadable('/usr/local/lib/libg3kbswitch.so') == 1 then
-        xkb_switch_lib = '/usr/local/lib/libg3kbswitch.so'
-    else
-        -- xkb-switch
-        local all_libs_locations = vim.fn.systemlist('ldd $(which xkb-switch)')
-        for _, value in ipairs(all_libs_locations) do
-            if string.find(value, 'libxkbswitch.so.1') or string.find(value, 'libxkbswitch.so.2') then
-                if string.find(value, 'not found') then
-                    xkb_switch_lib = nil
-                else
-                    xkb_switch_lib = string.sub(
-                        value, string.find(value, "/"), string.find(value, "%(") - 2
-                    )
+if vim.fn.executable('hyprland') == 1 then
+    local hyprland_devices_json = vim.fn.system('hyprctl devices -j')
+    local hyprland_devices = vim.json.decode(hyprland_devices_json)
+    for _, keyboard in pairs(hyprland_devices.keyboards) do
+        if keyboard.main == true then
+            hyprland_main_keyboard = keyboard
+            local index = 0
+            for word in string.gmatch(keyboard.layout, '([^,]+)') do
+                if word:match('^us') then
+                    user_us_layout_variation = index
                 end
+                index = index + 1
             end
+            break
         end
     end
 end
 
-if xkb_switch_lib == nil then
-    error("(xkbswitch.lua) Error occured: layout switcher file was not found.")
-end
-
-
-local function get_current_layout()
-    return vim.fn.libcall(xkb_switch_lib, 'Xkb_Switch_getXkbLayout', '')
-end
-
-local saved_layout = get_current_layout()
-local user_us_layout_variation = nil
-
-local user_layouts = vim.fn.systemlist(string.find(xkb_switch_lib, 'dylib') and 'issw -l' or
-    string.find(xkb_switch_lib, 'xkb') and 'xkb-switch -l' or string.find(xkb_switch_lib, 'g3kb') and 'g3kb-switch -l')
--- Find the used US layout (us/us(qwerty)/us(dvorak)/...)
-for _, value in ipairs(user_layouts) do
-    if string.find(value, user_os_name == 'Darwin' and 'ABC' or '^us') then
-        user_us_layout_variation = value
-    elseif string.find(value, '.US$') then
-        user_us_layout_variation = value
-    end
+if hyprland_main_keyboard == nil then
+    error("Could not detect `hyprland` or its main keyboard with `hyprctl devices -j`")
 end
 
 if user_us_layout_variation == nil then
-    error(
-        "(xkbswitch.lua) Error occured: could not find the English layout. Check your layout list. (xkb-switch -l / issw -l / g3kb-switch -l)")
+    error("Could not detect `us` layout of `" .. hyprland_main_keyboard.name .. "` in `hyprctl devices -j`")
+end
+
+local function get_current_layout()
+    return tonumber(vim.fn.system("hyprctl devices | sed -n '/^[[:space:]]*" .. hyprland_main_keyboard.name .. "$/,/active layout index:/ { /active layout index:/ s/.*:[[:space:]]*//p }'"))
+end
+
+local saved_layout = get_current_layout()
+
+local function set_layout(layout_index)
+    vim.fn.system('hyprctl switchxkblayout '.. hyprland_main_keyboard.name ..' ' .. layout_index)
 end
 
 function M.setup(opts)
 
-    -- Parse provided options
+   -- Parse provided options
     opts = opts or {}
     if opts.events_get_focus then
         M.events_get_focus = opts.events_get_focus
@@ -88,7 +63,7 @@ function M.setup(opts)
             callback = function()
                 vim.schedule(function()
                     saved_layout = get_current_layout()
-                    vim.fn.libcall(xkb_switch_lib, 'Xkb_Switch_setXkbLayout', user_us_layout_variation)
+                    set_layout(user_us_layout_variation)
                 end)
             end
         }
@@ -106,7 +81,7 @@ function M.setup(opts)
                     saved_layout = get_current_layout()
                     local current_mode = vim.api.nvim_get_mode().mode
                     if current_mode == "n" or current_mode == "no" or current_mode == "v" or current_mode == "V" or current_mode == "^V" then
-                        vim.fn.libcall(xkb_switch_lib, 'Xkb_Switch_setXkbLayout', user_us_layout_variation)
+                        set_layout(user_us_layout_variation)
                     end
                 end)
             end
@@ -122,7 +97,7 @@ function M.setup(opts)
             pattern = "*",
             callback = function()
                 vim.schedule(function()
-                    vim.fn.libcall(xkb_switch_lib, 'Xkb_Switch_setXkbLayout', saved_layout)
+                    set_layout(saved_layout)
                 end)
             end
         }
